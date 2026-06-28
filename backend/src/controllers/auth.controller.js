5d76b77286d9e9dd1b92db83313e45f1bd6e683a
alammd0 <sonualam96314@gmail.com>
2026-06-28 07:00:50 +0530

    some change

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -109,7 +109,8 @@ export const loginUser = async (req, res) => {
 
             res.cookie('token', token, {
                 httpOnly : true,
-                secure : true
+                secure: true,
+                sameSite: "none"
             });
 
             return res.status(200).json({
